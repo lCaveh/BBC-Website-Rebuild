04b61d87f8ff7a315636ebe3b00481c7dda60275
kaveh <lcaveh@gmail.com>
2019-01-25 14:36:52 -0800

    home component and its route added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,26 @@ import { AppComponent } from './app.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { PrimaryNavComponent } from './primary-nav/primary-nav.component';
 import { SubNavComponent } from './sub-nav/sub-nav.component';
+import { HomeComponent } from './home/home.component';
 
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: '', redirectTo: 'home',pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     MainNavComponent,
     PrimaryNavComponent,
-    SubNavComponent
+    SubNavComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
